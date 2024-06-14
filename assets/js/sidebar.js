471fdf767c90198b04ad47ee717fc45c409d23b4
Daniel <62035871+danielbarl@users.noreply.github.com>
2024-06-14 12:15:39 +0200

    Add sidebar functionality

---
+++ assets/js/sidebar.js
@@ -0,0 +1,17 @@
+document.getElementById('aboutLink').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('aboutSidebar').style.left = '1px';
+});
+
+document.getElementById('closeSidebar').addEventListener('click', function() {
+    const sidebar = document.getElementById('aboutSidebar');
+    sidebar.style.left = `-${sidebar.offsetWidth + 1}px`;
+});
+
+
+
+// Set initial position to fully off-screen
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('aboutSidebar');
+    sidebar.style.left = `-${sidebar.offsetWidth + 1}px`;
+});
